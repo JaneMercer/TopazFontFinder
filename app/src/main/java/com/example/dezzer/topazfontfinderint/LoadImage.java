00d14efd719b68d3426a8458da51bdbcff25a4b6
JaneMercer <lusy.neama@gmail.com>
2017-06-21 18:29:42 +0300

    image_processing

--- app/src/main/java/com/example/dezzer/topazfontfinderint/LoadImage.java
+++ app/src/main/java/com/example/dezzer/topazfontfinderint/LoadImage.java
@@ -2,10 +2,13 @@ package com.example.dezzer.topazfontfinderint;
 
 import android.Manifest;
 import android.annotation.TargetApi;
+import android.content.ActivityNotFoundException;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.database.Cursor;
+import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
@@ -20,17 +23,40 @@ import android.view.MenuItem;
 import android.util.Log;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Toast;
 import android.widget.ViewFlipper;
 
+import org.opencv.android.BaseLoaderCallback;
+import org.opencv.android.LoaderCallbackInterface;
 import org.opencv.android.OpenCVLoader;
+import org.opencv.android.Utils;
+//import org.opencv.core.Core;
+import org.opencv.core.Core;
+import org.opencv.core.Mat;
+import org.opencv.core.MatOfPoint;
+import org.opencv.core.MatOfPoint2f;
+import org.opencv.core.Point;
+import org.opencv.core.Rect;
+import org.opencv.core.Scalar;
+import org.opencv.core.Size;
+import org.opencv.imgproc.Imgproc;
+
+import  org.opencv.photo.Photo;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+
+
+import static org.opencv.core.CvType.CV_8UC3;
 
 public class LoadImage extends AppCompatActivity {
 
-    private static int RESULT_LOAD_IMAGE = 1;
+  //  private static int RESULT_LOAD_IMAGE = 1;
+  final int PIC_CROP = 1;
     private static final int MY_PERM_REQ_READ = 101;
-
     private static final int MY_PERM_REQ_WRITE = 102;
-
     private static final String TAG = "LoadImg";
 
     static{
@@ -67,7 +93,7 @@ public class LoadImage extends AppCompatActivity {
                         Intent.ACTION_PICK,
                         android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
 
-                startActivityForResult(i, RESULT_LOAD_IMAGE);
+                startActivityForResult(i, 0);
             }
         });
 
@@ -86,35 +112,95 @@ public class LoadImage extends AppCompatActivity {
     }
 
 
-
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
 
-
-        if (requestCode == RESULT_LOAD_IMAGE && resultCode == RESULT_OK && null != data) {
-            Uri selectedImage = data.getData();
-            String[] filePathColumn = { MediaStore.Images.Media.DATA };
+//        switch(requestCode){
+//            case 0: {
+        if (resultCode == RESULT_OK && null != data) {
+            final Uri selectedImage = data.getData();
+            String[] filePathColumn = {MediaStore.Images.Media.DATA};
 
             Cursor cursor = getContentResolver().query(selectedImage,
                     filePathColumn, null, null, null);
             cursor.moveToFirst();
 
             int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
-            String picturePath = cursor.getString(columnIndex);
+            final String picturePath = cursor.getString(columnIndex);
             cursor.close();
 
             findViewById(R.id.include1).setVisibility(View.GONE);
             findViewById(R.id.include2).setVisibility(View.VISIBLE);
 
-            ImageView imageView = (ImageView) findViewById(R.id.imgView);
-            imageView.setImageBitmap(BitmapFactory.decodeFile(picturePath));
+            //release mode
+//            ImageView imageView = (ImageView) findViewById(R.id.imgView);
+//            imageView.setImageBitmap(BitmapFactory.decodeFile(picturePath));
 
+            //OpenCV image preparation**********************************************
+            Bitmap bmp32 = JPGtoRGB888(BitmapFactory.decodeFile(picturePath));
+            Mat sImage = new Mat();
+            Mat grayImage = new Mat();
+            Mat blurImage = new Mat();
+            Mat thresImage = new Mat();
+            Mat binImage = new Mat();
+            Mat cannyImage = new Mat();
+            Bitmap temp = null;
+
+            Utils.bitmapToMat(bmp32, sImage);
+
+            Imgproc.cvtColor(sImage, grayImage, Imgproc.COLOR_BGR2GRAY);
+             Imgproc.GaussianBlur(grayImage,blurImage,new Size(3, 3),0);
+
+            Imgproc.adaptiveThreshold(blurImage, thresImage, 255, Imgproc.ADAPTIVE_THRESH_MEAN_C, Imgproc.THRESH_BINARY, 99, 4);
+            List<MatOfPoint> contours = new ArrayList<MatOfPoint>();
+            Mat hierarchy = new Mat();
+
+
+            Imgproc.Canny(thresImage, binImage, 10, 100, 3, true);
+
+            Imgproc.findContours(binImage, contours, hierarchy, Imgproc.RETR_TREE, Imgproc.CHAIN_APPROX_SIMPLE, new Point(0, 0));
+
+            hierarchy.release();
+            Imgproc.drawContours(binImage, contours, -1, new Scalar(255, 255, 255));//, 2, 8, hierarchy, 0, new Point());
+
+
+            MatOfPoint2f approxCurve = new MatOfPoint2f();
+
+            //For each contour found
+            for (int i = 0; i < contours.size(); i++) {
+                //Convert contours(i) from MatOfPoint to MatOfPoint2f
+                MatOfPoint2f contour2f = new MatOfPoint2f(contours.get(i).toArray());
+                //Processing on mMOP2f1 which is in type MatOfPoint2f
+                double approxDistance = Imgproc.arcLength(contour2f, true) * 0.02;
+                Imgproc.approxPolyDP(contour2f, approxCurve, approxDistance, true);
+
+                //Convert back to MatOfPoint
+                MatOfPoint points = new MatOfPoint(approxCurve.toArray());
+
+                // Get bounding rect of contour
+                Rect rect = Imgproc.boundingRect(points);
+
+                // draw enclosing rectangle (all same color, but you could use variable i to make them unique)
+                Imgproc.rectangle(binImage, new Point(rect.x, rect.y), new Point(rect.x + rect.width, rect.y + rect.height), new Scalar(255, 0, 0), 3);
+
+            }
+            approxCurve.release();
+
+            //debug mode-------------------------------
+            temp = Bitmap.createBitmap(binImage.cols(), binImage.rows(), Bitmap.Config.ARGB_8888);
+            Utils.matToBitmap(binImage, temp);
+
+            ImageView iv = (ImageView) findViewById(R.id.imgView);
+            iv.setImageBitmap(temp);
+            //----------------------------
+            //end of OpenCV image preparation**********************************************
+            sImage.release();
+            grayImage.release();
+            blurImage.release();
+            binImage.release();
         }
-
-
     }
-
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
@@ -138,8 +224,26 @@ public class LoadImage extends AppCompatActivity {
     }
 
 
+    private Bitmap JPGtoRGB888(Bitmap img){
+        Bitmap result = null;
+
+        int numPixels = img.getWidth() * img.getHeight();
+        int[] pixels = new int[numPixels];
+
+//        get jpeg pixels, each int is the color value of one pixel
+        img.getPixels(pixels,0,img.getWidth(),0,0,img.getWidth(),img.getHeight());
+
+//        create bitmap in appropriate format
+        result = Bitmap.createBitmap(img.getWidth(),img.getHeight(), Bitmap.Config.ARGB_8888);
+
+//        Set RGB pixels
+        result.setPixels(pixels, 0, result.getWidth(), 0, 0, result.getWidth(), result.getHeight());
+
+        return result;
+    }
 
 
+    //-----------------------------------------------------------------------------
     public void cancelBtn ( View view) {
         findViewById(R.id.include2).setVisibility(View.GONE);
         findViewById(R.id.include1).setVisibility(View.VISIBLE);
@@ -149,4 +253,10 @@ public class LoadImage extends AppCompatActivity {
         Intent intent = new Intent(this, characterList.class);
         startActivity(intent);
     }
+
+
 }
+
+
+
+
